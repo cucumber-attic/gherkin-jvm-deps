ed95bc87612238693d24a10bccac1ff808ecbdab
M.P. Korstanje <mpkorstanje@users.noreply.github.com>
2019-10-10 13:13:37 +0200

    Revert "gherkin-jvm-deps v1.1.x multi-release jar for jpms"

--- src/main/java11/module-info.java
+++
@@ -1,9 +0,0 @@
-module io.cucumber.gherkin.jvm.deps {
-    exports gherkin.deps.com.google.gson;
-    exports gherkin.deps.com.google.gson.annotations;
-    exports gherkin.deps.com.google.gson.reflect;
-    exports gherkin.deps.com.google.gson.stream;
-    exports gherkin.deps.net.iharder;
-    opens gherkin.deps.com.google.gson.internal to io.cucumber.gherkin;
-    requires java.sql;
-}
